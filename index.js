88ddd91954792252d5adc92a6edf14eef5d85857
William Dias <diasforsaken@gmail.com>
2015-12-21 13:32:13 -0500

    Minor. Undo.

--- index.js
+++ index.js
@@ -28,8 +28,7 @@ var openMobileApp = function () {
     var appName = getAppName(urlData);
     urlData = urlData.replace(appName + '/', '')
 
-    // window.open(appName + '://' + urlData, '_self');
-    window.open(appName + '://', '_self');
+    window.open(appName + '://' + urlData, '_self');
     return;
   }
 };
