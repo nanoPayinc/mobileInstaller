547a3f6d6dd8206aeffaceba4ba9e9d3d0d6e2ac
Michael Jonas <mjonas@Michaels-MacBook-Pro.local>
2016-05-11 15:11:39 -0400

    Handling error nessages and non-mobile clients

--- index.js
+++ index.js
@@ -12,23 +12,53 @@ var getAppName = function (urlData) {
   return urlData.split('/')[0];
 };
 
-var openMobileApp = function () {
+var isEmpty = function(str) {
+  return (!str || 0 === str.length);
+}
+
+var openAppOrWebpage = function () {
   var ua = navigator.userAgent;
   var params = getQueryParams();
   var urlData = window.location.href.split(window.location.host + '/')[1];
   var appName = getAppName(urlData);
-
+  var mobileRegex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+  var message = '';
+  
+  if(!(Object.keys(params).length === 0 && params.constructor === Object)) {
+    console.log('Not empty object', Object.keys(params));
+    message = decodeURIComponent(params.message);
+  }
+  
   var nanopay = document.getElementById('message');
   var mintchip = document.getElementById('message2');
+  var error = document.getElementById('errorMessage');
+  error.style.display = 'none';
 
   if (appName === 'mintchip') {
     nanopay.style.display = 'none';
     document.title = 'MintChip™';
     mintchip.style.display = 'block';
+    if(!(isEmpty(message))) {
+      error.getElementsByTagName("p")[0].innerHTML = message;
+      error.getElementsByTagName('img')[0].src = '/mintchip-logo.png';
+      mintchip.style.display = 'none';
+      error.style.display = 'block';
+    }
   } else {
     nanopay.style.display = 'block';
     document.title = 'nanoPay™';
     mintchip.style.display = 'none';
+    if(!(isEmpty(message))) {
+      error.getElementsByTagName("p")[0].innerHTML = message; 
+      error.getElementsByTagName("p")[0].style.color = '#9e0b39';
+      error.getElementsByTagName('img')[0].src = '/nanopay_logo.png';
+      nanopay.style.display = 'none';
+      error.style.display = 'block';
+    }
+  }
+  
+  if( !mobileRegex.test(ua) ) {
+    return;
   }
 
   if (ua.match(/Android/i)) {
@@ -50,5 +80,5 @@ var openMobileApp = function () {
   }
 };
 
-window.onload = openMobileApp;
+window.onload = openAppOrWebpage;
 
